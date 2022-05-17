86910c8afbacddb22b47c0c634dfee1ab4d48d68
Shelby Jueden <shelby@jueden.org>
2022-05-17 14:39:58 -0700

    Add remaining needed values with calculations and readouts

--- reform-v2.py
+++ reform-v2.py
@@ -10,31 +10,79 @@ from psu import PSU6633A
 from gui import GUI
 
 gui = GUI()
+#psu = PSU6633A(7)
+#dmm = DMM34401A(12)
 
+
+# Handle Ctrl+C closes of program
 def signal_handler(sig, frame):
     gui.end()
+    # Add PSU output disable
     print('You pressed Ctrl+C!')
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
-print("import test")
 
+def getPSUVolts():
+    return random.randint(5,12)
+
+def getPSUCurrent():
+    return random.randint(0,10)
+
+def getDMMCurrent():
+    return random.randint(0,20)
+
+
+d={}
+d["psu"]={}
+d["dmm"]={}
+d["r_limit"]={}
+d["cap"]={}
+
+d["psu"]["v"]=0
+d["psu"]["i"]=0
+d["dmm"]["i"]=0
+d["r_limit"]["r"]=220
 psu_v=12
 psu_i=2
 dmm_i=240
 
-gui.addReadout(psu_v,"Voltage","V","PSU")
-gui.addReadout(psu_i,"Current","mA","PSU")
+d["psu"]["v_id"]=gui.addReadout(psu_v,"Voltage","V","PSU")
+d["psu"]["i_id"]=gui.addReadout(psu_i,"Current","mA","PSU")
 
-dmm_i_id=gui.addReadout(dmm_i,"Current","uA","DMM")
-log_i=gui.addLogTable(["PSU Current","PSU Voltage","DMM Current","Cap Voltage","Cap Current"],"test.csv")
+d["dmm"]["v_id"]=gui.addReadout(dmm_i,"Current","uA","DMM")
+
+d["cap"]["v_id"]=gui.addReadout(dmm_i,"Voltage","V","Capacitor")
+d["cap"]["r_id"]=gui.addReadout(dmm_i,"Resistance","Ohms","Capacitor")
+
+
+log_i=gui.addLogTable(["PSU Voltage","PSU Current","DMM Current","Cap Voltage", "Cap Resistance"],"test.csv")
 
 
 while(True):
+
+    # Get new instrument readings
+    d["psu"]["v"]=getPSUVolts()
+    d["psu"]["i"]=getPSUCurrent()
+    d["dmm"]["i"]=getDMMCurrent()
+
+    d["r_limit"]["v_drop"]=d["dmm"]["i"]*d["r_limit"]["r"]
+    d["cap"]["v"]=d["psu"]["v"]-d["r_limit"]["v_drop"]
+    
+    try:
+        d["cap"]["r"]=d["cap"]["v"]/d["dmm"]["i"]
+    except ZeroDivisionError:
+        d["cap"]["r"]=0
+
+
     gui.update()
     time.sleep(0.1)
     dmm_i+=5
-    gui.updateReadout(dmm_i_id,dmm_i)
-    gui.updateLogTable(log_i,[random.randint(1,30),random.randint(1,30),dmm_i,random.randint(1,30),random.randint(1,30)])
+    gui.updateReadout(d["psu"]["v_id"],d["psu"]["v"])
+    gui.updateReadout(d["psu"]["i_id"],d["psu"]["i"])
+    gui.updateReadout(d["dmm"]["v_id"],d["dmm"]["i"])
+    gui.updateReadout(d["cap"]["v_id"],d["cap"]["v"])
+    gui.updateReadout(d["cap"]["r_id"],str(d["cap"]["r"])[:6])
+    gui.updateLogTable(log_i,[d["psu"]["v"],d["psu"]["i"],d["dmm"]["i"],d["cap"]["v"],d["cap"]["r"]])
 
