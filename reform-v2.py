0655afdfb1cb6b12d4c9ef44437d2a6f851dbebc
Shelby Jueden <shelby@jueden.org>
2023-03-26 14:18:01 -0700

    Add flask start for reforming

--- reform-v2.py
+++ reform-v2.py
@@ -6,6 +6,8 @@ import time
 import random
 from ctypes import *
 
+from flask import Flask
+
 from multiprocessing import Process, Value, Array
 
 
@@ -19,142 +21,184 @@ web_resistor = Value('d', 220.0)
 web_current_max = Value('d', 2000.0)
 web_current_min = Value('d', 150.0)
 web_log_name = Value(c_wchar_p,"Reform Log")
+control_reform = Value('d', 0.0)
+control_active = Value('d', 1)
 
 
 
 def reform():
-    gui = GUI()
-    psu = PSU6633A(12)
-    dmm = DMM34401A(7)
+    while(control_active.value):
+        time.sleep(0.5)
+        print("acvtive")
+        while (control_reform.value):
+            gui = GUI()
+            psu = PSU6633A(12)
+            dmm = DMM34401A(7)
 
-    psu.setCurrent(0.01)
-    psu.setVolt(1)
+            psu.setCurrent(0.01)
+            psu.setVolt(1)
 
-    dmm.setCurrent(0.01,0.000001)
+            dmm.setCurrent(0.01,0.000001)
 
-    # Handle Ctrl+C closes of program
-    def signal_handler(sig, frame):
-        gui.end()
-        # Add PSU output disable
-        psu.setCurrent(0)
-        psu.setVolt(0)
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
+            # Handle Ctrl+C closes of program
+            def signal_handler(sig, frame):
+                gui.end()
+                # Add PSU output disable
+                psu.setCurrent(0)
+                psu.setVolt(0)
+                print('You pressed Ctrl+C!')
+                sys.exit(0)
 
-    signal.signal(signal.SIGINT, signal_handler)
-
-
-    def getPSUVolts():
-        return psu.getVolt()
-
-    def getPSUCurrent():
-        return psu.getCurrent()
-
-    def getDMMCurrent():
-        i=dmm.read()*1000000
-        if i > 0:
-            return i
-        else:
-            return 0
+            signal.signal(signal.SIGINT, signal_handler)
 
 
-    d={}
-    d["psu"]={}
-    d["dmm"]={}
-    d["r_limit"]={}
-    d["cap"]={}
-    d["calc"]={}
+            def getPSUVolts():
+                return psu.getVolt()
 
-    d["psu"]["v"]=0
-    d["psu"]["i"]=0
-    d["dmm"]["i"]=0
-    d["calc"]["v"] = 0
+            def getPSUCurrent():
+                return psu.getCurrent()
 
-
-    d["calc"]["v_max"] = 16
-    d["r_limit"]["r"]=220
-    d["cap"]["i_max"]=2000 # Calculate next jump to 2mA
-    d["cap"]["i_min"]=150 # Calculate next jump to 2mA
+            def getDMMCurrent():
+                i=dmm.read()*1000000
+                if i > 0:
+                    return i
+                else:
+                    return 0
 
 
-    # Garbage UI init data
-    psu_v=12
-    psu_i=2
-    dmm_i=240
+            d={}
+            d["psu"]={}
+            d["dmm"]={}
+            d["r_limit"]={}
+            d["cap"]={}
+            d["calc"]={}
 
-    d["psu"]["v_id"]=gui.addReadout(psu_v,"Voltage","V","PSU")
-    d["psu"]["i_id"]=gui.addReadout(psu_i,"Current","mA","PSU")
-
-
-    d["calc"]["v_id"]=gui.addReadout(dmm_i,"Target Voltage","V","Controls")
-    d["calc"]["v_max_id"]=gui.addReadout(dmm_i,"Max Voltage","V","Controls")
-    d["cap"]["i_max_id"]=gui.addReadout(dmm_i,"Max Current","uA","Controls")
-    d["cap"]["i_min_id"]=gui.addReadout(dmm_i,"Min Current","uA","Controls")
-
-    d["dmm"]["v_id"]=gui.addReadout(dmm_i,"Current","uA","DMM")
-
-    d["cap"]["v_id"]=gui.addReadout(dmm_i,"Voltage","V","Capacitor")
-    d["cap"]["r_id"]=gui.addReadout(dmm_i,"Resistance","Ohms","Capacitor")
+            d["psu"]["v"]=0
+            d["psu"]["i"]=0
+            d["dmm"]["i"]=0
+            d["calc"]["v"] = 0
 
 
-    t = time.localtime()
-    log_i=gui.addLogTable(["PSU Voltage","PSU Current","Target Voltage","DMM Current","Cap Voltage", "Cap Resistance"],time.strftime('%Y-%m-%d_%H-%M-%S', t)+"_"+str(web_log_name.value)+".csv")
-
-    settle=10
-    while(d["psu"]["v"] < d["calc"]["v_max"]):
-
-        # Get new instrument readings
-        d["psu"]["v"]=getPSUVolts()
-        d["psu"]["i"]=getPSUCurrent()
-        d["dmm"]["i"]=getDMMCurrent()
-
-        d["r_limit"]["v_drop"]=(d["dmm"]["i"]/1000000)*d["r_limit"]["r"]
-        d["cap"]["v"]=d["psu"]["v"]-d["r_limit"]["v_drop"]
-
-        try:
-            d["cap"]["r"]=d["cap"]["v"]/(d["dmm"]["i"]/1000000)
-        except ZeroDivisionError:
-            d["cap"]["r"]=0
-
-        # Check for min current and calc voltage jump
-        if d["dmm"]["i"] < d["cap"]["i_min"]:
-            d["calc"]["v"] = d["cap"]["v"] + ((d["cap"]["i_max"]/1000000) * d["r_limit"]["r"])
-            psu.setVolt(d["calc"]["v"])
+            d["calc"]["v_max"] = 16
+            d["r_limit"]["r"]=220
+            d["cap"]["i_max"]=2000 # Calculate next jump to 2mA
+            d["cap"]["i_min"]=150 # Calculate next jump to 2mA
 
 
-        gui.update()
-        time.sleep(0.1)
-        dmm_i+=5
-        gui.updateReadout(d["psu"]["v_id"],d["psu"]["v"])
-        gui.updateReadout(d["psu"]["i_id"],d["psu"]["i"])
-        gui.updateReadout(d["calc"]["v_id"],d["calc"]["v"])
-        gui.updateReadout(d["calc"]["v_max_id"],d["calc"]["v_max"])
-        gui.updateReadout(d["cap"]["i_max_id"],d["cap"]["i_max"])
-        gui.updateReadout(d["cap"]["i_min_id"],d["cap"]["i_min"])
-        gui.updateReadout(d["calc"]["v_id"],d["calc"]["v"])
-        gui.updateReadout(d["dmm"]["v_id"],d["dmm"]["i"])
-        gui.updateReadout(d["cap"]["v_id"],d["cap"]["v"])
-        gui.updateReadout(d["cap"]["r_id"],str(d["cap"]["r"])[:6])
-        gui.updateLogTable(log_i,[d["psu"]["v"],d["psu"]["i"],d["calc"]["v"],d["dmm"]["i"],d["cap"]["v"],d["cap"]["r"]])
+            # Garbage UI init data
+            psu_v=12
+            psu_i=2
+            dmm_i=240
 
-        # Runaway current protection
-        if d["dmm"]["i"] > 2*d["cap"]["i_max"]:
-            if settle:
-                settle-=1
-            else:
-                break
+            d["psu"]["v_id"]=gui.addReadout(psu_v,"Voltage","V","PSU")
+            d["psu"]["i_id"]=gui.addReadout(psu_i,"Current","mA","PSU")
 
 
+            d["calc"]["v_id"]=gui.addReadout(dmm_i,"Target Voltage","V","Controls")
+            d["calc"]["v_max_id"]=gui.addReadout(dmm_i,"Max Voltage","V","Controls")
+            d["cap"]["i_max_id"]=gui.addReadout(dmm_i,"Max Current","uA","Controls")
+            d["cap"]["i_min_id"]=gui.addReadout(dmm_i,"Min Current","uA","Controls")
 
-    psu.setCurrent(0)
-    psu.setVolt(0)
-    dmm.beep()
+            d["dmm"]["v_id"]=gui.addReadout(dmm_i,"Current","uA","DMM")
+
+            d["cap"]["v_id"]=gui.addReadout(dmm_i,"Voltage","V","Capacitor")
+            d["cap"]["r_id"]=gui.addReadout(dmm_i,"Resistance","Ohms","Capacitor")
+
+
+            t = time.localtime()
+            log_i=gui.addLogTable(["PSU Voltage","PSU Current","Target Voltage","DMM Current","Cap Voltage", "Cap Resistance"],time.strftime('%Y-%m-%d_%H-%M-%S', t)+"_"+str(web_log_name.value)+".csv")
+
+            settle=10
+            while(d["psu"]["v"] < d["calc"]["v_max"]):
+
+                # Get new instrument readings
+                d["psu"]["v"]=getPSUVolts()
+                d["psu"]["i"]=getPSUCurrent()
+                d["dmm"]["i"]=getDMMCurrent()
+
+                d["r_limit"]["v_drop"]=(d["dmm"]["i"]/1000000)*d["r_limit"]["r"]
+                d["cap"]["v"]=d["psu"]["v"]-d["r_limit"]["v_drop"]
+
+                try:
+                    d["cap"]["r"]=d["cap"]["v"]/(d["dmm"]["i"]/1000000)
+                except ZeroDivisionError:
+                    d["cap"]["r"]=0
+
+                # Check for min current and calc voltage jump
+                if d["dmm"]["i"] < d["cap"]["i_min"]:
+                    d["calc"]["v"] = d["cap"]["v"] + ((d["cap"]["i_max"]/1000000) * d["r_limit"]["r"])
+                    psu.setVolt(d["calc"]["v"])
+
+
+                gui.update()
+                time.sleep(0.1)
+                dmm_i+=5
+                gui.updateReadout(d["psu"]["v_id"],d["psu"]["v"])
+                gui.updateReadout(d["psu"]["i_id"],d["psu"]["i"])
+                gui.updateReadout(d["calc"]["v_id"],d["calc"]["v"])
+                gui.updateReadout(d["calc"]["v_max_id"],d["calc"]["v_max"])
+                gui.updateReadout(d["cap"]["i_max_id"],d["cap"]["i_max"])
+                gui.updateReadout(d["cap"]["i_min_id"],d["cap"]["i_min"])
+                gui.updateReadout(d["calc"]["v_id"],d["calc"]["v"])
+                gui.updateReadout(d["dmm"]["v_id"],d["dmm"]["i"])
+                gui.updateReadout(d["cap"]["v_id"],d["cap"]["v"])
+                gui.updateReadout(d["cap"]["r_id"],str(d["cap"]["r"])[:6])
+                gui.updateLogTable(log_i,[d["psu"]["v"],d["psu"]["i"],d["calc"]["v"],d["dmm"]["i"],d["cap"]["v"],d["cap"]["r"]])
+
+                # Runaway current protection
+                if d["dmm"]["i"] > 2*d["cap"]["i_max"]:
+                    if settle:
+                        settle-=1
+                    else:
+                        break
+
+
+
+            psu.setCurrent(0)
+            psu.setVolt(0)
+            dmm.beep()
+            control_reform.value = 0
+
+
 
 
 procs = []
 proc = Process(target=reform)  # instantiating without any argument
 procs.append(proc)
+
 proc.start()
+
+app = Flask("The Reform Script")
+
+@app.route("/")
+def start_reform():
+
+    control_reform.value = 1
+    return " <p>Reform started</p>"
+
+@app.route("/kill")
+def kill():
+
+    control_active.value = 0
+    control_reform.value = 0
+    return " <p>killing process</p>"
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
+app.run(host="0.0.0.0")
+
+control_active.value = 0
 
 for proc in procs:
     proc.join()
